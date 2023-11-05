3b8b9e8bf8cd9097e1708649ddb3948433e12173
Irina <vyustirina@gmail.com>
2023-11-05 23:47:41 +0300

    added axios

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import React from "react";
 import "./index.css"
+ import Weather from "./Weather";
+
 
 function App() {
 
   return (
     <div className="App">
-     <div class="container">
-        <div class="weather-app-wrapper">
-        <div class="weather-app">
-            <form id="search-form" class="mb-4">
-                <div class="row">
-                    <div class="col-9">
-<input type="search" placeholder="Type a city.." class="form-control" id="city-input" autocomplete="off" />
-</div>
-<div class="col-3">
- <input type="submit" value="search" class="btn btn-outline-secondary w-100" />
-</div>
-</div>
-</form>
-            <div class="overview">
-    <h1 id="city">New York</h1>
-<ul>
-    <li> <span id="date"></span></li>
-    <li id="description">Clear</li>
-</ul>
-</div>
-<div class="row">
-    <div class="col-6">
-        <div class="d-flex weather-temperature">
-        <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="Clear" id="icon" />
-       <div> 
-        <strong id="temperature">12</strong>
-        <span class="units">
-            <a href="#" id="celsius-link" class="active">°C |</a> 
-            <a href="#" id="fahrenheit-link"> °F</a>
-        </span>
-    </div>
-    </div>
-    </div>
-    <div class="col-6">
-        <ul>
-        <li>Precipitation:<span id="precipitation"></span> 0%</li>
-            <li>Humidity:<span id="humidity"> 73</span>%</li>
-            <li >Wind: <span id="wind">1</span> km/h</li>
-        </ul>
-    </div>
-</div>
-<div class="weather-forecst" id="forecast">
+     <div className="container">
+        <div className="weather-app-wrapper">
+        <div className="weather-app">
+            
+      <Weather defaultCity="London" />      
+<div className="weather-forecst" id="forecast">
     
 </div>
 </div>
-<small> <a href="https://github.com/IrinaV1/vanilla-weather-app" target="_blank">Open-source code</a>, by Irina Vyust</small>
+<small> <a href="https://github.com/IrinaV1/vanilla-weather-app" rel="noreferrer" target="_blank">Open-source code</a>, by Irina Vyust</small>
 </div>
 </div>
     </div>
---
+++ src/Weather.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.css";
+import "./Weather.css";
+
+
+export default function Weather(props) {
+  
+    const [weatherData, setWeatherData] = useState({ready: false});
+    const apiKey = "2b6fdad0cbd018949c50c70f72250726";
+
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+
+    
+    function handleResponse(response){
+        setWeatherData({
+            ready: true,
+temperature: response.data.main.temp,
+humidity: response.data.main.humidity,
+date: "Wednesday 07:00",
+description: response.data.weather[0].description,
+iconUrl: "https://openweathermap.org/img/wn/10d@2x.png",
+wind: response.data.wind.speed,
+city: response.data.name
+});
+
+} 
+if (weatherData.ready) {
+
+    return (
+        <>
+        <form id="search-form" className="mb-4">
+                    <div className="row">
+                        <div className="col-9">
+    <input type="search" placeholder="Type a city.." className="form-control" id="city-input" autocomplete="off" />
+    </div>
+    <div className="col-3">
+     <input type="submit" value="search" className="btn btn-outline-secondary w-100" />
+    </div>
+    </div>
+    </form>
+        <div className="weather">
+    
+        <h1 id="city">{weatherData.city}</h1>
+    <ul>
+        <li> <span id="date">{weatherData.date}</span></li>
+        <li id="description">{weatherData.description}</li>
+    </ul>
+    </div>
+    <div className="row">
+        <div className="col-6">
+            <div className="d-flex weather-temperature">
+            <img src={weatherData.iconUrl} alt={weatherData.description} id="icon" />
+           <div> 
+            <strong id="temperature">{Math.round(weatherData.temperature)}</strong>
+            <span className="units">
+                <a href="/" id="celsius-link" className="active">°C |</ a> 
+                <a href="/" id="fahrenheit-link"> °F</a>
+            </span>
+        </div>
+        </div>
+        </div>
+        <div className="col-6">
+            <ul>
+            <li>Precipitation:<span id="precipitation"></span> 0%</li>
+                <li>Humidity:<span id="humidity"> 73</span>{weatherData.humidity} %</li>
+                <li >Wind: <span id="wind">1</span>{weatherData.wind} km/h</li>
+            </ul>
+        </div>
+    </div>
+    </>
+      );
+
+} else {
+
+ return <p>"Loading..."</p>;
+}
+};
